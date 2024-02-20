15564abb6f1ebe50ab1920d9b29db6ee1278c59f
bdvllrs <bdvllrs@gmail.com>
2024-02-20 08:22:38 +0000

    Add exports to __init__

--- migrate_ckpt/__init__.py
+++ migrate_ckpt/__init__.py
@@ -1,3 +1,19 @@
 import importlib.metadata
 
+from .migrate import (
+    CkptType,
+    Migration,
+    MigrationCallback,
+    ckpt_migration_key,
+    migrate_ckpt,
+)
+
 __version__ = importlib.metadata.version("migrate-ckpt")
+
+__all__ = [
+    "CkptType",
+    "Migration",
+    "MigrationCallback",
+    "ckpt_migration_key",
+    "migrate_ckpt",
+]
